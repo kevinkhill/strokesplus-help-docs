84c8c1c794a01f93b3496644d792799d673fd3f4
Kevin Hill <kevinkhill@gmail.com>
2021-02-22 20:11:33 -0800

    initial import

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,145 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <div id="background"></div>
+
+    <v-navigation-drawer app clipped temporary v-model="drawer">
+    </v-navigation-drawer>
+
+    <v-app-bar app color="indigo" dark clipped-left>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>Script Help</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>cloud</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <v-row>
+          <v-col :cols="leftCol">
+            <v-treeview
+              dense
+              hoverable
+              activatable
+              active-class="LB"
+              open-on-click
+              return-object
+              :items="items"
+            >
+              <template slot="label" slot-scope="{ item }">
+                <a @click="selectItem(item)">{{ item.name }}</a>
+              </template>
+            </v-treeview>
+          </v-col>
+
+          <v-col :cols="centerCol">
+            <v-container v-for="item in items" :key="item.id">
+              <p class="text-h4 ml-n4">{{ item.name }}</p>
+              <div v-if="item.children.length > 0">
+                <HelpEntry
+                  v-for="entry in item.children"
+                  :key="entry.id"
+                  :entry="entry"
+                ></HelpEntry>
+              </div>
+            </v-container>
+          </v-col>
+          <!--           
+          <v-col :cols="rightCol">
+          </v-col>   -->
+        </v-row>
+      </v-container>
+    </v-main>
+
+    <v-footer color="indigo" app>
+      <span class="white--text body-2">
+        Made by
+        <a href="http://kevinhill.codes" class="blue--text">Kevin Hill</a>
+      </span>
+    </v-footer>
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelpEntry from "./components/HelpEntry.vue";
+import help from "./help.json";
 
-export default {
-  name: 'App',
+@Component({
   components: {
-    HelloWorld
+    HelpEntry,
+  },
+})
+export default class App extends Vue {
+
+    drawer= false;
+    selection= [];
+    leftCol= 3;
+    centerCol= 9;
+    rightCol= 2;
+    search= null;
+    caseSensitive= false;
+  
+  selectItem(item) {
+    console.log(item);
   }
+  
+  items() {
+      let id = 1;
+
+      const sections = Object.entries(help).map(([key, section]) => {
+        let children = [];
+
+        if (help[key].Methods) {
+          const sectionMethods = Object.keys(help[key].Methods);
+
+          children = sectionMethods.map((methodName) => {
+            return {
+              id: id++,
+              name: methodName,
+              ...help[key].Methods[methodName],
+            };
+          });
+        }
+
+        return {
+          id: id++,
+          name: section.Name,
+          children,
+        };
+      });
+
+      return sections;
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.theme--light.v-application {
+  background: transparent !important;
+}
+
+/**
+ * Shamelessly stolen from strokesplus.net, Thanks Rob!
+ */
+#background {
+  z-index: -999;
+  position: fixed;
+  top: 56px;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url("./assets/StrokesPlus.net_NoBG.svg");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: contain;
+  background-position: center;
+  opacity: 0.05;
+  filter: alpha(opacity=05);
+}
+
+.LB {
+  border-left: 2px solid blue;
 }
 </style>
---
+++ src/components/HelpEntry.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-card class="mb-10">
+    <v-card-title>
+      <p class="font-weight-black text-h5">
+        <span class="font-italic font-weight-thin">sp.</span>{{ entry.Name }}()
+      </p>
+    </v-card-title>
+
+    <v-card-subtitle>
+      <span class="text--primary font-weight-bold"
+        >Returns:<span class="blue--text font-weight-thin"
+          >&nbsp;{{ entry.Returns }}</span
+        >
+      </span>
+    </v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      <div class="mb-2">
+        {{ entry.Description }}
+      </div>
+
+      <template v-if="Object.keys(entry.Parameters).length > 0">
+        <p class="text-h6 mt-8 mb-n2">Parameters</p>
+        <v-sheet elevation="1" rounded class="my-2 pa-2" v-if="entry.Parameters">
+          <v-simple-table dense>
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th class="text-left">Name</th>
+                  <th class="text-left">Type</th>
+                  <th class="text-left">Description</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item in entry.Parameters" :key="item.name">
+                  <td>{{ item.Name }}</td>
+                  <td>{{ item.Type }}</td>
+                  <td>{{ item.Description }}</td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </v-sheet>
+      </template>
+
+      <p class="text-h6 mt-8 mb-n2">Simple Example</p>
+      <v-sheet elevation="1" rounded class="my-2 pa-2" color="grey lighten-3">
+        <code class="grey lighten-3">
+          <pre>{{ entry.SimpleExample }}</pre>
+        </code>
+      </v-sheet>
+
+      <template v-if="entry.FullExample">
+        <p class="text-h6 mt-8 mb-n2">Full Example</p>
+        <v-sheet elevation="1" rounded class="my-2 pa-2" color="grey lighten-3">
+          <code class="grey lighten-3">
+            <pre>{{ entry.FullExample }}</pre>
+          </code>
+        </v-sheet>
+      </template>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+    HelpEntry,
+  },
+})
+export default class HelpEntry extends Vue {
+  @Prop()
+  entry: unknown;
+ 
+  mounted() {
+    console.log(`Mounted ${this.entry.Name}`);
+  }
+}
+</script>
+
+<style>
+code {
+  font-family: "Roboto Mono", monospace;
+  padding: 0 !important;
+}
+</style>
